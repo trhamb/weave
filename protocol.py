7bfae8e38ec4e13647510f0dcc0574f8608928c7
Tom Hamblin <tr.hamb@gmail.com>
2026-04-10 20:25:55 +0000

    Adding filepathing

--- protocol.py
+++ protocol.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 def build_response(parsed_request):
     is_valid, result = parsed_request
     if is_valid:
@@ -23,16 +26,15 @@ def parse_request(request_text):
         print(f"{stripped} - Invalid protocol: rejected")
         return (False, "Invalid request format")
 
-    if parts[0] == "WEAVE/1":
-        print(f"{stripped} - Valid protocol: accepted")
-        if parts[1][0] == '/':
-            print(f"{stripped} - address prefix correct")
-            return (True, parts[1])
-        else:
-            print(f"{stripped} - Invalid address")
-            return (False, "Invalid request")
-    else:
-        print(f"{stripped} - Invalid protocol: rejected")
+    if parts[0] != "WEAVE/1":
+        return (False, "Unsupported version")
+
+    if parts[1][0] != "/":
+        return (False, "Invalid path")
+
+    return (True, parts[1])
 
 
+def resolve_page(path):
+    pass
 
